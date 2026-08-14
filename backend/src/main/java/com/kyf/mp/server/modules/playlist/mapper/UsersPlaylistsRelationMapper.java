c76b00730b8e1035617269a98dfd778ecac428b6
kyf.zyk <2034315213@qq.com>
2026-08-14 20:15:27 +0800

    refactor(playlist): use explicit owner relation mapper

--- backend/src/main/java/com/kyf/mp/server/modules/playlist/mapper/UsersPlaylistsRelationMapper.java
+++ backend/src/main/java/com/kyf/mp/server/modules/playlist/mapper/UsersPlaylistsRelationMapper.java
@@ -1,16 +1,11 @@
 package com.kyf.mp.server.modules.playlist.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kyf.mp.server.modules.playlist.entity.UsersPlaylistsRelation;
+import org.apache.ibatis.annotations.Insert;
 
-/**
- * <p>
- * Mapper 接口
- * </p>
- *
- * @author kyf
- * @since 2026-04-05
- */
-public interface UsersPlaylistsRelationMapper extends BaseMapper<UsersPlaylistsRelation> {
+/** 用户创建歌单的关联写入。复合主键不适用 MyBatis-Plus 的按 ID CRUD。 */
+public interface UsersPlaylistsRelationMapper {
 
+    @Insert("INSERT INTO users_playlists_relation (user_id, playlist_id) VALUES (#{userId}, #{playlistId})")
+    int insert(UsersPlaylistsRelation relation);
 }
