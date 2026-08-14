c89b996ba270329900136bc9ec2dfe782ff4e6bf
kyf.zyk <2034315213@qq.com>
2026-08-14 20:04:15 +0800

    fix(auth): disable unused default security user

--- backend/src/main/java/com/kyf/mp/server/ServerApplication.java
+++ backend/src/main/java/com/kyf/mp/server/ServerApplication.java
@@ -3,8 +3,9 @@ package com.kyf.mp.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @MapperScan("com.kyf.mp.server.modules.**.mapper")
 public class ServerApplication {
 
