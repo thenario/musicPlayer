14cb1cd5344ef4cca87f578969683ae94f62c6ff
kyf.zyk <2034315213@qq.com>
2026-08-14 19:54:01 +0800

    chore(backend): clarify playlist song equality

--- backend/src/main/java/com/kyf/mp/server/modules/playlist/vo/PlaylistSongVO.java
+++ backend/src/main/java/com/kyf/mp/server/modules/playlist/vo/PlaylistSongVO.java
@@ -3,8 +3,10 @@ package com.kyf.mp.server.modules.playlist.vo;
 import com.kyf.mp.server.modules.song.entity.Songs;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class PlaylistSongVO extends Songs {
     private Integer songPlaylistPosition;
 }
