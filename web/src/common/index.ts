71451c1c3d9ee14e0e078257648eaceaf44ce53d
kyf.zyk <2034315213@qq.com>
2026-08-22 21:29:25 +0800

    chore(web): clear useless compnent

--- web/src/common/index.ts
+++ web/src/common/index.ts
@@ -1,7 +1,5 @@
-// 跨模块通用层：组件与 composables 统一从这里导出
-export { default as PageContainer } from './components/PageContainer.vue'
+// 跨模块通用层：composables 统一从这里导出
 export { default as AppPagination } from './components/AppPagination.vue'
-export { default as CopyText } from './components/CopyText.vue'
 export * from './composables/use-async-task'
 export * from './composables/use-pagination'
 export * from './composables/use-modal'
