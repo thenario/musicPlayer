85dcb54ed3796e94d6b491b23ab8638bfb561ba0
kyf.zyk <2034315213@qq.com>
2026-08-24 14:35:42 +0800

    refine(backend): add redis configs ttl and serialize

--- backend/src/main/java/com/kyf/mp/server/config/RedisConfig.java
+++ backend/src/main/java/com/kyf/mp/server/config/RedisConfig.java
@@ -1,10 +1,25 @@
 package com.kyf.mp.server.config;
 
+import java.time.Duration;
+
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
-
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new GenericJackson2JsonRedisSerializer(objectMapper)));
+    }
 }
