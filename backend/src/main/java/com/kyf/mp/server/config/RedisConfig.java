1ed5c4f990cd9f072c03559827da981a8e48768c
kyf.zyk <2034315213@qq.com>
2026-08-27 14:53:37 +0800

    config(redis):refine config

--- backend/src/main/java/com/kyf/mp/server/config/RedisConfig.java
+++ backend/src/main/java/com/kyf/mp/server/config/RedisConfig.java
@@ -9,17 +9,31 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+        ObjectMapper redisObjectMapper = objectMapper.copy();
+
+        redisObjectMapper.activateDefaultTyping(
+            BasicPolymorphicTypeValidator.builder()
+            .allowIfSubType("com.kyf.mp.server.")
+            .allowIfSubType("java.util.")
+            .allowIfSubType("java.time.")
+            .build(),
+            ObjectMapper.DefaultTyping.NON_FINAL,
+            JsonTypeInfo.As.PROPERTY
+        );
+
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .disableCachingNullValues()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-                        new GenericJackson2JsonRedisSerializer()));
+                        new GenericJackson2JsonRedisSerializer(redisObjectMapper)));
     }
 }
